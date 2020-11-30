9c0673e8b6e6484985242350fb971d874cbae1a2
Andrea Zotti <and.zotti@gmail.com>
2020-11-30 18:14:33 +0100

    acid page

---
+++ page-acid.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Template Name: Hyaluronic Acid template
+ *
+ * This is the template that displays the aboput page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Rosepharma
+ */
+
+get_header();
+?>
+
+	<main d="primary" class="wrapper full acid-page">
+
+	<div class="wrapper">
+			<section class="container full section-0">
+				<div class="container">
+					<div class="columns-wrapper display-section even">
+						<div class="box col-6">
+							<h2>Natural Hyaluronic Acid</h2>
+						</div>
+						<div class="box col-6 nopadding">
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/about-section-0.jpg" alt="">			
+						</div>
+					</div>
+				</div>
+		</section>
+	</div>
+
+	<div class="wrapper">
+			<section class="container full section-1">
+				<div class="container">
+					<div class="columns-wrapper display-section odd">
+						<div class="box col-6 ">
+							<h2>HYALURONIC ACID</h2>
+							<p>Hyaluronic acid, abbreviated to HA, is a natural element of your skin.As a result of skin ageing, this precious natural fi ller decreases and we can notice a loss of volume, compactness and smoothness.There are many solutions tailored by Rose Pharma to the needs of every skin type, age range and desired result, from facial cream with hyaluronic acid to injectable hyaluronic acid fillers.</p>
+						</div>						
+						<div class="box col-6">
+						</div>
+					</div>
+				</div>
+		</section>
+	</div>	
+
+
+	<div class="wrapper">
+		<section class="container full">
+			<div class="container">
+				<div class="columns-wrapper display-section even">
+					<div class="box col-6 content">
+						<p>Providing high quality tools is an essential part of our
+mission.
+
+Our BD Hylock syringes for professional hyaluronic acid
+injection are the result of advanced research:
+• Glass Syringe
+• Steam Sterilization
+• Pre-fi llable
+• Easy to use
+• BD Hylok LLA resists a pull-out force 3Xhigher
+• BD Hylok LLA resists rotational torque 5Xhigher
+• Superior Performances
+
+Our syringes allow professionals to work with maximum
+comfort and obtain the expected result after a fl awless
+procedure.</p>
+					</div>
+					<div class="box col-6 content">
+						<p>Our TSK needles are the complement of our syringes.
+They combine ULTRA Thin Wall technology with the
+unique HPC Advance Hub geometry.
+
+Their combined characteristics allow our needles to have
+the best possible fl ow rate.
+
+Each professional hyaluronic acid injection becomes
+easier and more comfortable, making treatment more
+accurate and improving the experience for patients.</p>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+	</div>
+	
+	<div class="wrapper">
+		<section class="container full">
+			<div class="container">
+				<div class="columns-wrapper display-section odd">
+					<div class="box col-12 center">
+						<h2>Rose Pharma</h2>
+						<p>About Rose Pharma: the company<br/>
+
+ROSE PHARMA SA is an Italian company currently based in Lugano, Switzerland. The company was established in 2011, mainly
+focusing on aesthetic treatments for dermatology, gynaecology and plastic surgery.</p>
+
+<p>
+About Rose Pharma: R&D<br/>
+Our company is focused on research, development and manufacturing of hyaluronic acid based dermal fi llers and innovative
+dermal aesthetic treatments. Our hyaluronic acid dermal fi llers are premium products, specially developed to off er a range of
+functions that meet the main problems related to skin ageing. Our research and development department is actively working to
+bring new solutions and updates that represent the state of the art of the reference sector.</p>
+
+<p>
+About Rose Pharma: our goals<br/>
+Our main goals are to revitalize natural beauty and help regaining the physiological functionality of the tissues. In order to
+achieve our goals we explore all the benefi cial properties of hyaluronic acid based global treatments. Moving along the line of
+medical treatments that meet the needs of patients and doctors, we off er cutting-edge solutions in line with the reference market.</p>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+	</div>	
+	
+
+		<div class="wrapper">
+			<section class="container full section-8">
+				<div class="container">
+					<div class="columns-wrapper display-section even">
+						<div class="box col-4">
+							<h3>hyaluronic Acid</h3>
+							<p>Premium product, global care: our claim describes the key points of <b>Rose Pharma - Zhoabex</b> hyaluronic acid dermal fi llers and complementary treatments.Find our <b>professional information</b> in the dedicated area.</p>
+							<a class="body-link" href="#"><span>Find out</span></a>							
+						</div>
+						<div class="box col-4">
+							<h3>Did you know?</h3>
+							<p>Premium product, global care: our claim describes the key points of <b>Rose Pharma - Zhoabex</b> hyaluronic acid dermal fi llers and complementary treatments.Find our <b>professional information</b> in the dedicated area.</p>
+							<a class="body-link" href="#"><span>Find out</span></a>
+						</div>
+						<div class="box col-4">
+							<h3>About us</h3>
+							<p>Premium product, global care: our claim describes the key points of <b>Rose Pharma - Zhoabex</b> hyaluronic acid dermal fi llers and complementary treatments.Find our <b>professional information</b> in the dedicated area.</p>
+							<a class="body-link" href="#"><span>Find out</span></a>
+						</div>
+					</div>
+				</div>
+		</section>
+	</div>
+	
+	<div class="wrapper">
+		<section class="container full section-9">
+			<div class="container">
+				<div class="columns-wrapper display-section odd">
+					<div class="box col-4 center">
+							<h2>Contact us</h2>
+							<h3>For more information</h3>
+							<a class="body-link" href="#"><span>Contact us</span></a>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+	</div>				
+
+	</main><!-- #main -->
+
+<?php
+//get_sidebar();
+get_footer();
