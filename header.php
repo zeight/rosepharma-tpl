0686e9876ebd1dc26f977b6ef4ca3ec19d469969
Andrea Zotti <and.zotti@gmail.com>
2020-12-28 12:21:20 +0100

    header link to home

--- header.php
+++ header.php
@@ -12,30 +12,35 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site page-wrapper">
-	<div class="container full">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'rosepharma' ); ?></a>
+  <?php wp_body_open(); ?>
+  <div id="page" class="site page-wrapper">
+    <div class="container full">
+      <a class="skip-link screen-reader-text"
+        href="#primary"><?php esc_html_e( 'Skip to content', 'rosepharma' ); ?></a>
 
-		<header id="masthead" class="site-header wrapper">
-			<div class="container">
-				<div class="columns-wrapper">
-					<div class="box col-12  header-logo-container">
-						<img src="<?php bloginfo('template_url'); ?>/assets/img/rosepharma_logo.svg" alt="">
-					</div>
-					<div class="box col-12 navigation-container">
-					<nav id="site-navigation" class="main-navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php //esc_html_e( 'Primary Menu', 'rosepharma' ); ?></button>
-						<?php
+      <header id="masthead" class="site-header wrapper">
+        <div class="container">
+          <div class="columns-wrapper">
+            <div class="box col-12  header-logo-container">
+              <a href="<?php echo get_home_url() ?>">
+                <img src="<?php bloginfo('template_url'); ?>/assets/img/rosepharma_logo.svg" alt="">
+              </a>
+            </div>
+            <div class="box col-12 navigation-container">
+              <nav id="site-navigation" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu"
+                  aria-expanded="false"><?php //esc_html_e( 'Primary Menu', 'rosepharma' ); ?></button>
+                <?php
 						wp_nav_menu(
 						array(
 						'theme_location' => 'menu-1',
@@ -43,28 +48,28 @@
 						)
 						);
 						?>
-					</nav><!-- #site-navigation -->
+              </nav><!-- #site-navigation -->
 
-					</div>
-				</div>
-			</div>
-			<!-- <div class="site-branding"> -->
-				<?php
+            </div>
+          </div>
+        </div>
+        <!-- <div class="site-branding"> -->
+        <?php
 				//the_custom_logo();
 				//if ( is_front_page() && is_home() ) :
 					?>
-					<!-- <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
-					<?php
+        <!-- <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
+        <?php
 				//else :
 					?>
-					<!-- <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p> -->
-					<?php
+        <!-- <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p> -->
+        <?php
 				//endif;
 				?>
-			<!-- </div> -->
-			<!-- .site-branding -->
+        <!-- </div> -->
+        <!-- .site-branding -->
 
 
-		</header><!-- #masthead -->
+      </header><!-- #masthead -->
 
-	</div>
+    </div>
